89dbdf0c66b00335ba2c1f201a4d79ded137eb59
fcrozatier <frederic.crozatier@protonmail.com>
2023-06-16 20:02:59 +0200

    configure shiki in browser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,48 @@
 <script lang="ts">
+	import { getHighlighter, loadTheme } from 'shiki';
 	import type { PageData } from './$types';
 	import { marked } from 'marked';
+	import { markedHighlight } from 'marked-highlight';
+	import { escapeSvelte } from 'mdsvex';
+
+	marked.use(
+		markedHighlight({
+			async: true,
+			highlight(code, lang = 'text') {
+				return getHighlighter({ theme: 'nord', langs: ['python'] }).then((hl) => {
+					return escapeSvelte(hl.codeToHtml(code, { lang }));
+				});
+			}
+		})
+	);
 
 	export let data: PageData;
 
-	const html = marked.parse(data.content);
-	console.log('html:', html);
+	let html: string;
+	$: console.log('html:', html);
+
+	const md = `
+# Real good post
+
+This is a post
+
+\`\`\`python
+def add(a,b):
+  return a+b
+\`\`\`
+`;
+
+	html = marked.parse(md);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{@html html}
+{#await html}
+	parsing
+{:then code}
+	{@html code}
+{/await}
 
 <style lang="postcss">
 	p {
