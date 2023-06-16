f509bd4e39ff4c6f9c2ebcc0eb8930f1f3db7dc8
fcrozatier <frederic.crozatier@protonmail.com>
2023-06-16 19:48:19 +0200

    configure mdsvex with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-import { mdsvex } from 'mdsvex';
+import { escapeSvelte, mdsvex } from 'mdsvex';
+import { getHighlighter } from 'shiki';
 
 /** @type {import('mdsvex').MdsvexOptions;} */
-export const mdsvexOptions = { extensions: ['.md'] };
+export const mdsvexOptions = {
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const hl = await getHighlighter({ theme: 'poimandres' });
+			const html = escapeSvelte(hl.codeToHtml(code, { lang }));
+			return `{@html \`${html}\`}`;
+		}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
