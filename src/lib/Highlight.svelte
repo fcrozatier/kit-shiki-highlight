073a077ab9249a730cf9339944601eef0416ca96
fcrozatier <frederic.crozatier@protonmail.com>
2023-06-19 11:00:04 +0200

    create highlight component

---
+++ src/lib/Highlight.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+	import { getHighlighter, setWasm } from 'shiki';
+
+	const wasmResponse = await fetch('/shiki/wasm/onig.wasm');
+	setWasm(wasmResponse);
+
+	export const highlighter = await getHighlighter({
+		theme: 'dark-plus',
+		langs: ['python'],
+		paths: { languages: '/shiki/languages', themes: '/shiki/themes' }
+	});
+</script>
+
+<script lang="ts">
+	export let code = '';
+	export let language = 'python';
+</script>
+
+{@html highlighter.codeToHtml(code, { lang: language })}
