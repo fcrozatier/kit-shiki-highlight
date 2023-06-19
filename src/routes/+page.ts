9ae81cc999187cbbffd5b63911f4f19c81c092ba
fcrozatier <frederic.crozatier@protonmail.com>
2023-06-19 10:08:57 +0200

    add link

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,1 @@
-import type { PageLoad } from './$types';
-
 export const ssr = false;
-
-const paths = import.meta.glob('/src/posts/first.md', { eager: true, as: 'raw' });
-
-export const load: PageLoad = async () => {
-	const first = Object.entries(paths)[0];
-	console.log('res:', first[1]);
-
-	return { content: first[1] };
-};
---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Highlight from '$lib/Highlight.svelte';
+
+	const md = `
+# Title
+
+Paragraph
+
+\`\`\`python
+def add(a,b):
+  return a+b
+\`\`\`
+`;
+
+	let markdown = [md];
+
+	function add() {
+		markdown = [...markdown, md];
+	}
+</script>
+
+<h1>Reuse highlight</h1>
+
+<button on:click={add}>Add</button>
+
+{#each markdown as md}
+	<Highlight markdown={md} />
+{/each}
