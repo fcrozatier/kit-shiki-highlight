79dbc35ba80e81b963af2f31d3a35574c4cce8cf
fcrozatier <frederic.crozatier@protonmail.com>
2023-06-19 10:00:33 +0200

    add themes

--- src/posts/first.md
+++
@@ -1,8 +0,0 @@
-# Real good post
-
-This is a post
-
-```python
-def add(a,b):
-  return a+b
-```
--- src/routes/api/posts/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const data = import.meta.glob('/src/posts/first.md');
-	console.log('data:', data);
-
-	return new Response();
-};
